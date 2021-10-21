8ad42ffbdc5c30befd4ba27ba4366286bf7cdb46
Luis RoaH <luis.roa@avaldigitallabs.com>
2021-10-20 23:29:11 -0500

    Add function incremen equipo uno y equipo dos

--- app/src/main/java/co/org/poli/futbolgolapp/MatchActvity.java
+++ app/src/main/java/co/org/poli/futbolgolapp/MatchActvity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import co.org.poli.Entity.Match;
 
@@ -12,8 +15,10 @@ public class MatchActvity extends AppCompatActivity {
 
     private TextView firstTeam;
     private TextView secondTeam;
+    private TextView countOne;
+    private TextView countTwo;
     private Intent dataMainActivity;
-    private Match  match;
+    private Match match;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +27,60 @@ public class MatchActvity extends AppCompatActivity {
 
         firstTeam = findViewById(R.id.textNameF);
         secondTeam = findViewById(R.id.textNameS);
+        countOne = findViewById(R.id.textContOne);
+        countTwo = findViewById(R.id.textContTwo);
 
         //Get Intent mainActivity
         dataMainActivity = getIntent();
-        match =  (Match) dataMainActivity.getSerializableExtra("currentMatch");
+        match = (Match) dataMainActivity.getSerializableExtra("currentMatch");
 
         firstTeam.setText(match.getHomeTeam().getNameTeam());
         secondTeam.setText(match.getVisitingTeam().getNameTeam());
+        countOne.setText(String.valueOf(match.getHomeTeamGoals()));
+        countTwo.setText(String.valueOf(match.getVisitingTeamGoals()));
     }
 
+    public void addPointOneE1(View view) {
+        dataMainActivity = getIntent();
+        match = (Match) dataMainActivity.getSerializableExtra("currentMatch");
+        match.setHomeTeamGoals(match.getHomeTeamGoals() + 1);
+        countOne.setText(String.valueOf(match.getHomeTeamGoals()));
 
+    }
+
+    public void addPointTwoE1(View view) {
+        dataMainActivity = getIntent();
+        match = (Match) dataMainActivity.getSerializableExtra("currentMatch");
+        match.setHomeTeamGoals(match.getHomeTeamGoals() + 2);
+        countOne.setText(String.valueOf(match.getHomeTeamGoals()));
+    }
+
+    public void addPointThreeE1(View view) {
+        dataMainActivity = getIntent();
+        match = (Match) dataMainActivity.getSerializableExtra("currentMatch");
+        match.setHomeTeamGoals(match.getHomeTeamGoals() + 3);
+        countOne.setText(String.valueOf(match.getHomeTeamGoals()));
+    }
+
+    public void addPointOneE2(View view) {
+        dataMainActivity = getIntent();
+        match = (Match) dataMainActivity.getSerializableExtra("currentMatch");
+        match.setVisitingTeamGoals(match.getVisitingTeamGoals() + 1);
+        countTwo.setText(String.valueOf(match.getVisitingTeamGoals()));
+
+    }
+
+    public void addPointTwoE2(View view) {
+        dataMainActivity = getIntent();
+        match = (Match) dataMainActivity.getSerializableExtra("currentMatch");
+        match.setVisitingTeamGoals(match.getVisitingTeamGoals() + 2);
+        countTwo.setText(String.valueOf(match.getVisitingTeamGoals()));
+    }
+
+    public void addPointThreeE2(View view) {
+        dataMainActivity = getIntent();
+        match = (Match) dataMainActivity.getSerializableExtra("currentMatch");
+        match.setVisitingTeamGoals(match.getVisitingTeamGoals() + 3);
+        countTwo.setText(String.valueOf(match.getVisitingTeamGoals()));
+    }
 }
